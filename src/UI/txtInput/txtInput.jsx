d3da3a8b266284d7efba2b12547255dac6d1adc5
Fedchenko Artem <artem_fedchenko_2017@mail.ru>
2024-07-02 16:02:46 +0700

    fix: text input styles

--- src/UI/txtInput/txtInput.jsx
+++ src/UI/txtInput/txtInput.jsx
@@ -8,6 +8,7 @@ export const TxtInput = ({ labelTxt, setter, actionType, newValueName }) => {
 				type="text"
 				className={st.inputGroup_input}
 				required
+				maxLength={100}
 			/>
 			<label className={st.inputGroup_placeholder}>
 				<p className={st.inputGroup_txt}>{labelTxt}</p>
